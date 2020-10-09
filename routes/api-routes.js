8aa3a3acd3594e84121d69503070649545aebde4
Steven Schaab <stevenes83@gmail.com>
2020-10-08 19:20:10 -0500

    changed production DB

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,7 +19,6 @@ module.exports = function (app) {
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
     db.User.create({
-      // memberid: "",
       username: req.body.email,
       email: req.body.email,
       password: req.body.password,
