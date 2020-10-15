26bb8a21b31968c7dfba7f966bf9b7d1e2735378
Prerona Mukherjee <prerona24@gmail.com>
2020-10-14 21:33:14 -0500

    gardens

---
+++ public/js/gardens.js
@@ -0,0 +1,54 @@
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function showSucc(){
+      var y =document.getElementById("succulent");
+      if (y.style.display === "block") {
+        y.style.display = "none";
+      } else {
+        y.style.display = "block";
+      }
+    }
+
+    function showSummer(){
+        var z =document.getElementById("summer");
+        if (z.style.display === "block") {
+          z.style.display = "none";
+        } else {
+          z.style.display = "block";
+        }
+      }
+  
+      function showFall(){
+        var fallPlant =document.getElementById("fall");
+        if (fallPlant.style.display === "block") {
+          fallPlant.style.display = "none";
+        } else {
+          fallPlant.style.display = "block";
+        }
+      }
+  
+      function show(){
+        var a =document.getElementById("plants");
+        if (a.style.display === "block") {
+          a.display = "none";
+        } else {
+          a.display = "block";
+        }
+      }
+      
+      function showSpring(){
+        var spring =document.getElementById("spring");
+        if (spring.style.display === "block") {
+          spring.style.display = "none";
+        } else {
+          spring.style.display = "block";
+        }
+      }
+  
