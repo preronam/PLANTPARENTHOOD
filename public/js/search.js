a2755a41104bdd1d12b7739641cd158f3b42fe4f
Mari <marinarosemartin@gmail.com>
2020-10-23 21:44:26 -0500

    updated search.js using handlebars

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var plants = [];
+
     //When the form is submitted, the posts are done
     $(".search").on("submit", function (event) {
         event.preventDefault();
@@ -15,29 +15,11 @@ $(document).ready(function () {
             type: "GET",
             url: url
         }).then(function (response) {
-            $("#results-card").empty();
-            let html = "";
-            response.forEach(plant => {
-                html = `  
-                <ul>
-                <li id="plant">Plant Name: ${plant.Common_Name}</li>
-                <li id="sci">Scientific Name: ${plant.Scientific_Name}</li>
-                <li id="sunlight">Sunlight Needs: ${plant.Sunlight_Needs}</li>
-                <li id="humid">Humidity Needs: ${plant.Humidity}</li>
-                <li id="moisture">Soil Moisture Needs: ${plant.Moisture_Levels}</li>
-                <li id="poison">Toxic: ${plant.Poisonous}</li>
-                <li id="maintain">Maintenance Level: ${plant.Maintenance_Level}</li>
-                <li id="price">Price Range: ${plant.Price}</li>
-                </ul>`;
-            });
-            $('#results-card').append(html);
-
+            console.log(response);
+            // window.location = "/";
         });
-
     });
-
 });
-
 
 
 
